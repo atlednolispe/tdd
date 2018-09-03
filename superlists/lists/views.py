f2c89e6f6f9850c10a876001cb22532019af9085
atlednolispe <atlednolispe@gmail.com>
2018-09-03 23:00:35 +0800

    New list accompanys with capture group.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -9,8 +9,9 @@ def home_page(request):
     return render(request, 'home_page.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(
         request,
         'list.html',
@@ -20,4 +21,4 @@ def view_list(request):
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % list_.id)
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -22,7 +22,7 @@ from lists.views import (
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', view_list, name='view_list'),
+    url(r'^lists/(.+)/$', view_list, name='view_list'),
     url(r'^lists/new$', new_list, name='new_list')
     # url(r'^admin/', admin.site.urls),
 ]
