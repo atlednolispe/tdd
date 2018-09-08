eec2fceeb38fd3586fc953a41b255950212ef295
atlednolispe <atlednolispe@gmail.com>
2018-09-08 13:40:46 +0800

    Basic deploy.

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -20,19 +20,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^xkjqvjw1ee&l^l(n6bet=y&)x6=ib6r6m4$id)6mt6$iv)4x5'
+# SECRET_KEY = '^xkjqvjw1ee&l^l(n6bet=y&)x6=ib6r6m4$id)6mt6$iv)4x5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
-#
-# TEMPLATE_DEBUG = False
 
-
-# docker nginx
-# docker run -d -p 80:80 -v /home/atlednolispe/Github/tdd/configurations/nginx:/etc/nginx/conf.d -v /home/atlednolispe/Github/tdd/static:/tmp/static nginx
-# gunicorn superlists.wsgi:application --bind 0.0.0.0:8000
-ALLOWED_HOSTS = ['172.17.0.1']
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or '^xkjqvjw1ee&l^l(n6bet=y&)x6=ib6r6m4$id)6mt6$iv)4x5'
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
