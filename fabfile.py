e7c812bb3099ef3c1c1f0b901ea4b074e86dc234
atlednolispe <atlednolispe@gmail.com>
2018-09-12 11:53:49 +0800

    Change fabric to fabric3.

--- fabfile.py
+++ fabfile.py
@@ -1,54 +1,79 @@
 """
 fabric无法sudo写入/etc/systemd/system/gunicorn-superlists-staging.service
 
-修改HOST和USER以及IDENTITY_FILE
+部署命令
+fab deploy -u [user] -H [ip] -i [identity_file]
+Example:
+    fab deploy -u product -H 192.168.123.110 -i ~/Password/dell/product.pem
+
+之后按照输出在远程机器上执行下面类似的5条命令
+==============================
+Success set configuration. Please run the following 3 command to run the site!
+
+1. Please add the service config!
+sed 's/USER/product/' /home/product/site/configurations/service/gunicorn-superlists-staging.service | sed 's,VIRTUALENVPATH,/home/product/.local/share/virtualenvs/site-Pa3_Gi3K,' | sudo tee /etc/systemd/system/gunicorn-superlists-staging.service
+
+2. Please run the docker nginx.
+docker run -d -p 80:80 -v /home/product/site/configurations/nginx:/etc/nginx/conf.d -v /home/product/site/static:/superlists/static -v /tmp:/tmp nginx
+
+3. Please reload the service!
+sudo systemctl daemon-reload
+
+4. Please start the service!
+sudo systemctl start gunicorn-superlists-staging
+
+5. Please check the service!
+systemctl status gunicorn-superlists-staging
 """
 import random
 
-from fabric import Connection
-
+# from fabric import Connection
+from fabric.api import (
+    run, env
+)
 
 REPO_URL = 'https://github.com/atlednolispe/tdd.git'
-HOST = '192.168.123.110'
-USER = 'product'
-IDENTITY_FILE = '/home/atlednolispe/Password/dell/product.pem'
+HOST = env.hosts[0]
+USER = env.user
+IDENTITY_FILE = env.key_filename[0]
 
 
 def deploy():
-    conn = Connection(host=HOST, user=USER, connect_kwargs={'key_filename': IDENTITY_FILE})
-
     command_mkdir_site = 'mkdir -p site'
-    conn.run(command_mkdir_site)
+    run(command_mkdir_site)
 
     command_git_clone = f'git clone {REPO_URL} ~/site/'
-    conn.run(command_git_clone)
+    run(command_git_clone)
 
     # .profile .bashrc .bash_profile, SET PATH都没用..
     command_pipenv_sync = f'cd /home/{USER}/site && /home/{USER}/.local/bin/pipenv sync'
-    conn.run(command_pipenv_sync)
+    run(command_pipenv_sync)
 
     command_ls_virtual_python = f'ls /home/{USER}/.local/share/virtualenvs/ | grep site-'
-    virtualenv_name = conn.run(command_ls_virtual_python).stdout.strip()
+    virtualenv_name = run(command_ls_virtual_python).stdout.strip()
     virtualenv_path = f'/home/{USER}/.local/share/virtualenvs/{virtualenv_name}'
     virtualenv_python = f'{virtualenv_path}/bin/python'
 
     cd_superlist_workon_path = f'cd /home/{USER}/site/superlists'
     command_collectstatic = f'{cd_superlist_workon_path} && {virtualenv_python} manage.py collectstatic'
-    conn.run(command_collectstatic)
+    run(command_collectstatic)
 
     command_make_database_dir = f'mkdir -p /home/{USER}/site/database'
-    conn.run(command_make_database_dir)
+    run(command_make_database_dir)
 
     command_migrate = f'{cd_superlist_workon_path} && {virtualenv_python} manage.py migrate'
-    conn.run(command_migrate)
+    run(command_migrate)
 
     # 设置.env环境变量
-    chars = ''.join(chr(i) for i in range(33, 122))
+    # chr(34) = '"',会导致echo写入错误
+    chars = ''.join(chr(i) for i in range(33, 122) if i != 34)
     key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-    command_add_env = f'echo -e "DJANGO_DEBUG_FALSE=y\nSITENAME=192.168.123.110\nDJANGO_SECRET_KEY={key}" > /home/{USER}/site/superlists/.env'
-    conn.run(command_add_env)
+    command_add_env = f'echo -e "DJANGO_DEBUG_FALSE=y\nSITENAME={HOST}\nDJANGO_SECRET_KEY={key}" > /home/{USER}/site/superlists/.env'
+    run(command_add_env)
 
-    print('Success set configuration. Please run the following 3 command to run the site!\n')
+    print()
+    print('='*30)
+    print('Success set configuration. Please run the following 5 command to run the site!\n')
 
     command_sed_service = f"sed 's/USER/product/' /home/{USER}/site/configurations/service/gunicorn-superlists-staging.service | sed 's,VIRTUALENVPATH,{virtualenv_path},' | sudo tee /etc/systemd/system/gunicorn-superlists-staging.service"
     print('1. Please add the service config!')
@@ -72,6 +97,8 @@ def deploy():
 
     command_check_service_status = 'systemctl status gunicorn-superlists-staging'
     print('5. Please check the service!')
+    print(command_check_service_status)
+    print()
     print('if Active: active (running) = Success')
     print("""Example:
     
@@ -84,8 +111,8 @@ def deploy():
                ├─18814 /home/product/.local/share/virtualenvs/site-Pa3_Gi3K/bin/python3.6m /home/product/.local/share/virtualenvs/site-Pa3_Gi3K/bin/gunicorn --bind unix:/tmp/superlists-staging.socket superlis
                └─18835 /home/product/.local/share/virtualenvs/site-Pa3_Gi3K/bin/python3.6m /home/product/.local/share/virtualenvs/site-Pa3_Gi3K/bin/gunicorn --bind unix:/tmp/superlists-staging.socket superlis
     """)
+    print('=' * 30)
     print()
-    print(command_check_service_status)
 
 
 if __name__ == '__main__':
